af00d65d5f4a6b152e497091f067802f35112242
amiivas <amitsrikiet@gmail.com>
2021-07-14 09:25:04 +0530

    Update createProviderOrganization.sh

--- scripts/product_scripts/apic/createProviderOrganization.sh
+++ scripts/product_scripts/apic/createProviderOrganization.sh
@@ -17,13 +17,15 @@ var=$?
 echo "exit code: $var"
 echo "2"
 
-#Creating cluster endpoint
-apic_server=$apic_release_name-mgmt-admin-$namespace.apps.$cluster_name.$domain_name
 
-echo "APIC Admin Endpoint :: ${apic_server}"
 
-#Getting Realms : Not needed as of now. taking default value
-#apic identity-providers:list --scope admin --server ${apic_server}
+#Getting Management server 
+apic_server=$(oc -n ${namespace} get mgmt ${apic_release_name}-mgmt -o jsonpath="{.status.zenRoute}" && echo "")
+
+echo "INFO: APIC Management Server Endpoint URL : $apic_server"
+
+#To get realms
+#apic identity-providers:list --scope admin --server "${apic_server}" --fields title,realm
 
 if [[ "$user" == "" ]]; then
   echo "Using default user as admin"
@@ -44,18 +46,31 @@ if [[ "$catalog" == "" ]]; then
   catalog=sandbox
 fi
 
-#Accepting apic licenses
-echo "Accepting apic licenses"
-apic --accept-license
-sleep 2
-apic --live-help
+#Accepting /home/azureuser/apic licenses
+echo "Accepting /home/azureuser/apic licenses"
+/home/azureuser/apic --accept-license --live-help
 sleep 2
 #Logging to API Connect CMC as admin
-apic login --username admin --password "${password}" --server ${apic_server} --realm admin/default-idp-1
+/home/azureuser/apic login --username admin --password "${password}" --server ${apic_server} --realm admin/default-idp-1
 echo
 sleep 5
+
+#Getting API Manager local user registry URL
+echo "INFO: Getting API Manager local user registry URL"
+api_manager_lur_url=$(/home/azureuser/apic user-registries:get --server  ${apic_server} --org admin api-manager-lur --format json --output - | jq -r '.url')
+
+#Adding API manager local user registry to API Manager for provider org creation
+echo "INFO: Getting API Manager default user registry URL"
+default_provider_url=$(/home/azureuser/apic user-registry-settings:get --server ${apic_server} --format json --output - | jq -r .provider_user_registry_default_url)
+
+cat << EOF | /home/azureuser/apic user-registry-settings:update --server ${apic_server} -
+provider_user_registry_urls: 
+  - "${default_provider_url}"
+  - "${api_manager_lur_url}"
+EOF
+
 #Creating default user for API Manager
-output=$(cat << EOF | apic users:create --server ${apic_server} --org ${user} --user-registry api-manager-lur -
+output=$(cat << EOF | /home/azureuser/apic users:create --server ${apic_server} --org ${user} --user-registry api-manager-lur -
 username: apiadmin
 email: amit.srivastav@cognizant.com
 first_name: APIManager
@@ -71,7 +86,7 @@ echo "Owner URL: ${URL}"
 
 sleep 5
 #Creating Provider Organization
-orgoutput=$(cat << EOF | apic orgs:create --server ${apic_server} -
+orgoutput=$(cat << EOF | /home/azureuser/apic orgs:create --server ${apic_server} -
 name: ${org}
 title: ${org}
 owner_url: ${URL}
@@ -82,13 +97,13 @@ sleep 5
 echo "Output ${orgoutput}"
 
 #Getting Organization Id
-orgResp=$(apic orgs:get --server ${apic_server} ${org} --fields id --output -)
+orgResp=$(/home/azureuser/apic orgs:get --server ${apic_server} ${org} --fields id --output -)
 sleep 2
 orgid=$(echo $orgResp | cut -d' ' -f 2)
 echo "Org Id : $orgResp   : $orgid"
 ret=0
 if [[ "$orgid" == "" ]]; then
-  orgResp=$(apic orgs:get --server ${apic_server} ${org} --fields id --output -)
+  orgResp=$(/home/azureuser/apic orgs:get --server ${apic_server} ${org} --fields id --output -)
   sleep 2
   orgid=$(echo $orgResp | cut -d' ' -f 2)
   echo "Org Id : $orgResp   : $orgid  retry $ret"
@@ -101,7 +116,7 @@ fi
 #Getting Portal ID and Portal Service URL
 
 
-portalResponse=$(apic portal-services:list --server ${apic_server} --org admin --availability-zone availability-zone-default)
+portalResponse=$(/home/azureuser/apic portal-services:list --server ${apic_server} --org admin --availability-zone availability-zone-default)
 sleep 5
 portalURL=$(echo ${portalResponse} | cut -d' ' -f 2)
 
@@ -132,19 +147,19 @@ sleep 5
 #Setting mail server
 echo "Setting Demo Mail Server .. "
 
-cat << EOF | apic mail-servers:create --org admin --server ${apic_server}  -
+cat << EOF | /home/azureuser/apic mail-servers:create --org admin --server ${apic_server}  -
 title: demo-email-server
 name: demo-email-server
 host: smtp.sendgrid.net
 port: 587
 credentials:
   username: apikey
-  password: 12345
+  password: SG.Q2zQUTXTTcGqF6iTzhtVXA.V18213X6iHyHHbnMdJ3GoHW040zXkx9uQzkdv6qMTVk
 EOF
 
 
 sleep 1
-mail_server=$(apic mail-servers:get --server  ${apic_server} --org admin demo-email-server --output - --fields url)
+mail_server=$(/home/azureuser/apic mail-servers:get --server  ${apic_server} --org admin demo-email-server --output - --fields url)
 echo $mail_server
 mail_server_url=$(echo $mail_server | cut -d' ' -f 3)
 echo "mail server url : $mail_server_url"
@@ -153,35 +168,36 @@ echo "Updating cloud settings with email server ... "
 cat << EOF > cloud_config.yaml
 mail_server_url: ${mail_server_url}
 email_sender:
-  name: APIC Administrator
+  name: /home/azureuser/apic Administrator
   address: amitsrikiet@gmail.com
 
 EOF
 
-apic cloud-settings:update --server ${apic_server} cloud_config.yaml
+/home/azureuser/apic cloud-settings:update --server ${apic_server} cloud_config.yaml
 echo "Logging out admin from CMC"
-apic logout --server ${apic_server}
+/home/azureuser/apic logout --server ${apic_server}
 
 sleep 5
 
 echo "Logging as newly created user apiadmin in Organization ${org} in API Manager"
-apic login --server ${apic_server} --username apiadmin --password "cts@1234" --realm provider/default-idp-2
+/home/azureuser/apic login --server ${apic_server} --username apiadmin --password "cts@1234" --realm provider/default-idp-2
 echo
 
 sleep 5
 echo "Gateway available for the organizaton"
-apic gateway-services:list --server ${apic_server} --scope org --org ${org}
+/home/azureuser/apic gateway-services:list --server ${apic_server} --scope org --org ${org}
 
 echo "Updating catalog settings for portal services"
-apic catalog-settings:update --org ${org} --server ${apic_server} --catalog ${catalog} portal_config.yaml
+/home/azureuser/apic catalog-settings:update --org ${org} --server ${apic_server} --catalog ${catalog} portal_config.yaml
 
 echo "Publishing Products ..."
-apic products:publish --server ${apic_server} --org ${org} --catalog sandbox --accept-license --live-help products/cts-demo-apic-product_1.0.0.yaml
+/home/azureuser/apic products:publish --server ${apic_server} --org ${org} --catalog sandbox --accept-license --live-help products/cts-demo-/home/azureuser/apic-product_1.0.0.yaml
    
 echo  "Uploading API in API Manager Drafts"
-apic draft-apis:create --server ${apic_server} --org ${org} products/cts-demo-apic-api_1.0.0.yaml
+/home/azureuser/apic draft-apis:create --server ${apic_server} --org ${org} products/cts-demo-/home/azureuser/apic-api_1.0.0.yaml
 
 sleep 4
-apic logout --server ${apic_server}
+/home/azureuser/apic logout --server ${apic_server}
 
-#yes | sh publish-products.sh ${cluster_name} ${domain_name} ${namespace} ${apic_release_name} ${org} apiadmin "cts@1234"
+yes | sh publish-products.sh ${cluster_name} ${domain_name} ${namespace} ${apic_release_name} ${org} apiadmin "cts@1234" ${apic_server}
+
