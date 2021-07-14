ac5be1c287b584857081fc304ca993d0c2aa9da9
amiivas <amitsrikiet@gmail.com>
2021-07-14 09:25:29 +0530

    Update publishProducts.sh

--- scripts/product_scripts/apic/publishProducts.sh
+++ scripts/product_scripts/apic/publishProducts.sh
@@ -7,6 +7,7 @@ export apic_release_name=$4
 export org=$5
 export user=$6
 export password=$7
+export apic_server=$8
 
 if [[ "$org" == "" ]]; then
   org=cts-demo
@@ -14,23 +15,18 @@ fi
 
 whoami
 
-#Creating cluster endpoint
 echo '************* Inside publish-products.sh ***************'
-apic_server=$apic_release_name-mgmt-admin-$namespace.apps.$cluster_name.$domain_name
-resp=$(apic --accept-license)
-sleep 2
-lh=$(apic --live-help)
+resp=$(/home/azureuser/apic --accept-license --live-help)
 sleep 2
 
 
 echo "Logging to API Manager :: ${apic_server} for user ${user} and password ${password}"
 
 
-
 sleep 5
 products_folder_path="./products/"
 cd ${products_folder_path}
-apic login --server ${apic_server} --username ${user} --password ${password} --realm provider/default-idp-2
+/home/azureuser/apic login --server ${apic_server} --username ${user} --password ${password} --realm provider/default-idp-2
 echo "Products Folder Path ${products_folder_path}" 
 for FILE in *product*; 
 do 
@@ -38,8 +34,7 @@ do
      echo  "Publishing $FILE"
      
      echo "User logged in : " 
-     apic me:get --server ${apic_server} --accept-license --live-help
-     apic products:publish --server ${apic_server} --org ${org} --catalog sandbox --accept-license --live-help cts-demo-apic-product_1.0.0.yaml
+     /home/azureuser/apic products:publish --server ${apic_server} --org ${org} --catalog sandbox cts-demo-apic-product_1.0.0.yaml
      var=$?
      
      if [[ var -eq 0 ]]; then
@@ -50,20 +45,20 @@ do
      echo "No Products to publish !!."
    fi
 done
-#sleep 5
-#echo "Uploading APIs in draft state in API Manager"
-#for FILE in *; 
-#do 
-#   if [[ -f "$FILE" ]]; then
-#     echo  "Uploading $FILE"
-#     apic draft-apis:create --server ${apic_server} --org ${org} $FILE
-#     var=$?
-#     if [[ var -eq 0 ]]; then
-#       mkdir -p ../draftapis
-#       mv $FILE ../draftapis/.
-#     fi
-#   else 
-#     echo "No APIs to upload !!."
-#   fi
-#done
+sleep 5
+echo "Uploading APIs in draft state in API Manager"
+for FILE in *; 
+do 
+   if [[ -f "$FILE" ]]; then
+     echo  "Uploading $FILE"
+     /home/azureuser/apic draft-apis:create --server ${apic_server} --org ${org} $FILE
+     var=$?
+     if [[ var -eq 0 ]]; then
+       mkdir -p ../draftapis
+       mv $FILE ../draftapis/.
+     fi
+   else 
+     echo "No APIs to upload !!."
+   fi
+done
 
