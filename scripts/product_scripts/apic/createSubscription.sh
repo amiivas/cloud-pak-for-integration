0a5585bc899aee7eb42659ece81a7489bb6ab97d
amiivas <amitsrikiet@gmail.com>
2021-07-14 09:24:32 +0530

    Update createSubscription.sh

--- scripts/product_scripts/apic/createSubscription.sh
+++ scripts/product_scripts/apic/createSubscription.sh
@@ -6,8 +6,9 @@ export namespace=$3
 export apic_release_name=$4
 export org=$5
 export catalog=$6
-export user=$6
-export password=$7
+export user=$7
+export password=$8
+export apic_server=$9
 
 #Creating consumer app file
 echo "username: consumerapp">consumerapp.txt
@@ -16,9 +17,6 @@ echo "first_name: Consumer">>consumerapp.txt
 echo "last_name: Demo">>consumerapp.txt
 echo "password: cts@1234">>consumerapp.txt
 
-apic_server=$apic_release_name-mgmt-admin-$namespace.apps.$cluster_name.$domain_name
-
-echo "APIC Admin Endpoint :: ${apic_server}"
 
 if [[ "$user" == "" ]]; then
   echo "Using default user as admin"
@@ -39,17 +37,17 @@ if [[ "$catalog" == "" ]]; then
   catalog=sandbox
 fi
 
-apic login --server ${apic_server} --username ${user} --password ${password} --realm provider/default-idp-2
+/home/azureuser/apic login --server ${apic_server} --username ${user} --password ${password} --realm provider/default-idp-2
 
 echo "Creating new user in sandbox catalog"
 
-res=$(apic users:create --server ${apic_server} --org ${org} --user-registry sandbox-catalog consumerapp.txt)
+res=$(/home/azureuser/apic users:create --server ${apic_server} --org ${org} --user-registry sandbox-catalog consumerapp.txt)
 
 sid=$(echo ${res} | cut -d' ' -f 4)
 
 echo $sid
 
-consumer_org_res=$(cat << EOF | apic consumer-orgs:create --server ${apic_server} --org ${org} --catalog ${catalog} -
+consumer_org_res=$(cat << EOF | /home/azureuser/apic consumer-orgs:create --server ${apic_server} --org ${org} --catalog ${catalog} -
 name: cts-demo-consumer-org
 title: cts-demo-consumer-org
 owner_url: ${sid}
@@ -59,7 +57,7 @@ EOF
 echo $consumer_org_res
 
 #Creating new app in the consumer organization
-app_res=$(cat << EOF | apic apps:create --consumer-org cts-demo-consumer-org --catalog ${catalog} --server ${apic_server} --org ${org} -
+app_res=$(cat << EOF | /home/azureuser/apic apps:create --consumer-org cts-demo-consumer-org --catalog ${catalog} --server ${apic_server} --org ${org} -
 title: cts-consumer-app
 EOF
 )
@@ -67,7 +65,7 @@ EOF
 echo "Consumer App response $app_res"
 
 #Getting client id and secret 
-credResp=$(apic credentials:get --server ${apic_server} --catalog ${catalog} --org ${org} --consumer-org cts-demo-consumer-org --app cts-consumer-app credential-for-cts-consumer-app --fields client_id,client_secret --output -)
+credResp=$(/home/azureuser/apic credentials:get --server ${apic_server} --catalog ${catalog} --org ${org} --consumer-org cts-demo-consumer-org --app cts-consumer-app credential-for-cts-consumer-app --fields client_id,client_secret --output -)
 
 echo "Credentials ... "
 echo $credResp
